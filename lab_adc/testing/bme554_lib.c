0f640acc2c5cced2a249a11e7596fe391ec1feec
Sophie Pelton <sophie.pelton@duke.edu>
2026-03-26 12:08:09 -0400

    adc testing first draft

--- lab_adc/testing/bme554_lib.c
+++ lab_adc/testing/bme554_lib.c
@@ -1,4 +1,13 @@
 #include "bme554_lib.h"
 
 K_EVENT_DEFINE(program_test_events);
+
+/* GPIO lab */
 int student_frequency;
+
+/* ADC lab — Phase 1 */
+int   student_adc_mv;
+float student_mapped_freq;
+
+/* ADC lab — Phase 2/3 */
+int student_calc_cycles_result;
